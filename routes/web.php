0d983694527c1460ae14b2e8ec62f8d521334f94
saroj4195 <sarojkumarpatel12@gmail.com>
2024-11-23 12:22:49 +0530

    refactor

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Foundation\Application;
 */
 
 $router->get('/', function () use ($router) {
-    return app()->version();
+    return "Hello";
 });
 //Hotel User Registration Route
 $router->post('/hotel_users/register', ['uses' => 'CompanyRegistrationController@registerHotelAdmin']);
