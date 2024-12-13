45849533ddfac06bacd1528dae64b9d2264a8734
Saroj Kumar Patel <sarojkumarpatel12@gmail.com>
2024-12-13 11:11:23 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Foundation\Application;
 $router->get('/', function () use ($router) {
     // return $router->app->version();
 
-    return app()->version();
+    return "dibya";
 });
 //Hotel User Registration Route
 $router->post('/hotel_users/register', ['uses' => 'CompanyRegistrationController@registerHotelAdmin']);
